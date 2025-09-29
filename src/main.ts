a5d8ca3dce902d4800f9bd4180cb63dea6960b93
Sandeep1816 <99470339+Sandeep1816@users.noreply.github.com>
2025-09-29 11:21:21 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ✅ Enable CORS for frontend
+  app.enableCors({
+    origin: 'http://localhost:3000', // frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
